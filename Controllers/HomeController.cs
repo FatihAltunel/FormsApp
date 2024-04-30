de5812ecac85a99867b43ae836d9cd60d8d5c689
Fatih Altunel <jjrousseau1128@gmail.com>
2024-04-30 15:09:34 +0300

    Filter added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FormsApp.Controllers;
 
 public class HomeController : Controller
 {
     [HttpGet]
-    public IActionResult Index(string searchString)
+    public IActionResult Index(string searchString, string category)
     {
 
         var Products = Repository.Products;
 
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+
         if(!String.IsNullOrEmpty(searchString)){
             ViewBag.searchString = searchString;    
             Products = Products.Where(p => p.Name.ToLower().Contains(searchString.ToLower())).ToList();
+        }
+
+        if(!String.IsNullOrEmpty(category) && category!="0"){
+            Products = Products.Where(p => p.CategoryId == int.Parse(category)).ToList();
         }
 
         return View(Products);
